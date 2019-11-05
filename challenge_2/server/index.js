d06960b798fb4467ad24f5c6cc5ee644ed1e2320
Manuel Duran <mduran85@gmail.com>
2019-11-05 11:11:27 -0600

    restructured files

--- challenge_2/server/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = require('./server.js');
-var port = 3000;
-
-app.listen(port, () => {
-  console.log(`server running on port:${port}`)
-})
--- challenge_3/server/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = require('./server.js');
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`server listening on ${port}`);
-})
