b071fb022e4e3d84ecd6be8f20d955ff5d832b84
Manuel Duran <mduran85@gmail.com>
2020-02-20 17:56:30 -0600

    built client

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(logger('dev'));
-
 app.use(express.static(path.join(__dirname, '../public')))
 
 
